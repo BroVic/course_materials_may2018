e120330c98caac1fe1b8046ca9c1bc4b2a7a8d36
Victor Ordu <victorordu@outlook.com>
2018-06-12 19:33:14 +0100

    Initial commit

---
+++ week7/declarations-definitions-2018-06-12-BroVic-master/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "utility.h"
+
+
+
+//Leave main() alone! THAT MEANS DON'T TOUCH CODE BELOW THIS LINE!!!
+int main() {
+	std::cout << "Enter a name: ";
+	auto name = ReadStringFromConsole();
+	std::cout << "Enter an adjective: ";
+	auto adjt = ReadStringFromConsole();
+
+	std::cout << DescribePersonAsSomething(name, adjt) << '\n';
+	return 0;
+}
+
